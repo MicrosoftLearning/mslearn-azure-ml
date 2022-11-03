c398382efb21e2b99e8d15b0f542f5f326e4681b
madiepev <madiepev@microsoft.com>
2022-11-03 12:19:42 +0100

    update

--- Labs/10/setup.sh
+++ Labs/10/setup.sh
@@ -10,7 +10,7 @@ az ml workspace create --name "mlw-dp100-labs" -g "rg-dp100-labs"
 # Create compute instance
 RANDOM=$$
 
-ComputeName="ci-${RANDOM}"
+ComputeName="ci${RANDOM}"
 
 echo "Creating a compute instance with name: " $ComputeName
 az ml compute create --name ${ComputeName} --size STANDARD_DS11_V2 --type ComputeInstance -w mlw-dp100-labs -g rg-dp100-labs
