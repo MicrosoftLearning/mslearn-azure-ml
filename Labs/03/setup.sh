e3bd825c62914bbc99cd5f68530ae646e2d66f4b
madiepev <madiepev@microsoft.com>
2023-05-09 14:17:29 +0200

    fix data save bug

--- Labs/03/setup.sh
+++ Labs/03/setup.sh
@@ -1,11 +1,28 @@
 #! /usr/bin/sh
 
-# Create workspace
-echo "Create a resource group:"
-az group create --name "rg-dp100-labs" --location "eastus"
+# Set the necessary variables
+RESOURCE_GROUP="rg-dp100-labs"
+RESOURCE_PROVIDER="Microsoft.MachineLearning"
+REGIONS=("eastus" "westus" "centralus" "northeurope" "westeurope" "southeastasia")
+RANDOM_REGION=${REGIONS[$RANDOM % ${#REGIONS[@]}]}
+WORKSPACE_NAME="mlw-dp100-labs"
+
+# Register the Azure Machine Learning resource provider in the subscription
+echo "Register the Machine Learning resource provider:"
+az provider register --namespace $RESOURCE_PROVIDER
+
+# Delete the resource group if it already exists to avoid clashes
+echo "Delete the resource group" $RESOURCE_GROUP "to avoid clashes:"
+az group delete --name $RESOURCE_GROUP
+
+# Create the resource group and workspace and set to default
+echo "Create a resource group and set as default:"
+az group create --name $RESOURCE_GROUP --location $RANDOM_REGION
+az configure --defaults group=$RESOURCE_GROUP
 
 echo "Create an Azure Machine Learning workspace:"
-az ml workspace create --name "mlw-dp100-labs" -g "rg-dp100-labs"
+az ml workspace create --name $WORKSPACE_NAME 
+az configure --defaults workspace=$WORKSPACE_NAME 
 
 # Create compute instance
 guid=$(cat /proc/sys/kernel/random/uuid)
@@ -15,8 +32,8 @@ suffix=${suffix:0:18}
 ComputeName="ci${suffix}"
 
 echo "Creating a compute instance with name: " $ComputeName
-az ml compute create --name ${ComputeName} --size STANDARD_DS11_V2 --type ComputeInstance -w mlw-dp100-labs -g rg-dp100-labs
+az ml compute create --name ${ComputeName} --size STANDARD_DS11_V2 --type ComputeInstance 
 
 # Create compute cluster
 echo "Creating a compute cluster with name: aml-cluster"
-az ml compute create --name "aml-cluster" --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute -w mlw-dp100-labs -g rg-dp100-labs
+az ml compute create --name "aml-cluster" --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
---
+++ typings/__builtins__.pyi
@@ -0,0 +1,2 @@
+
+from databricks.sdk.runtime import *
