80dcbb88d8d4a59171936c6aab72dd8f7390a67b
SkillableKarl <65239900+SkillableKarl@users.noreply.github.com>
2025-07-24 12:19:44 -0700

    Create setup1.sh

---
+++ Labs/09/setup1.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/sh
+
+# Check if a suffix was passed as an argument
+if [[ -n "$1" ]]; then
+  suffix=$1
+else
+  # Generate random suffix from UUID if none was provided
+  guid=$(cat /proc/sys/kernel/random/uuid)
+  suffix=${guid//[-]/}
+  suffix=${suffix:0:18}
+fi
+
+echo "Suffix: $suffix"
+
+# Set the necessary variables
+RESOURCE_GROUP="rg-dp100-l${suffix}"
+RESOURCE_PROVIDER="Microsoft.MachineLearningServices"
+REGIONS=("eastus" "westus" "centralus" "northeurope" "westeurope")
+RANDOM_REGION=${REGIONS[$RANDOM % ${#REGIONS[@]}]}
+WORKSPACE_NAME="mlw-dp100-l${suffix}"
+COMPUTE_INSTANCE="ci${suffix}"
+COMPUTE_CLUSTER="aml-cluster"
+
+# Register the Azure Machine Learning resource provider in the subscription
+echo "Register the Machine Learning resource provider:"
+az provider register --namespace $RESOURCE_PROVIDER
+
+# Create the resource group and workspace and set to default
+echo "Create a resource group and set as default:"
+az group create --name $RESOURCE_GROUP --location $RANDOM_REGION
+az configure --defaults group=$RESOURCE_GROUP
+
+echo "Create an Azure Machine Learning workspace:"
+az ml workspace create --name $WORKSPACE_NAME 
+az configure --defaults workspace=$WORKSPACE_NAME 
+
+# Create compute instance
+echo "Creating a compute instance with name: " $COMPUTE_INSTANCE
+az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_DS11_V2 --type ComputeInstance 
+
+# Create compute cluster
+echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
+
+# Create data assets
+echo "Create training data asset:"
+az ml data create --type uri_file --name "diabetes-data" --path ./data/diabetes.csv 
